8494af8c0f58e84caff49da815002802b59da51a
DimMalysh <d.malysh@mail.ru>
2018-03-17 17:02:07 +0200

    Adapter

--- Adapter.playground/Contents.swift
+++ Adapter.playground/Contents.swift
@@ -1,5 +1,38 @@
-// Adapter
+//Simple example to use the pattern Adapter for adapting european measure of length from american measure.
 
-import UIKit
+protocol MeasureOfLength {
+    var kilometer: Double {get}
+}
 
-var str = "Hello, playground"
+class  EuropeanMeasureOfLength : MeasureOfLength {
+    var kilometer : Double
+    
+    init(kilometer : Double) {
+        self.kilometer = kilometer
+    }
+}
+
+class AmericanMeasureOfLength {
+    var mile : Double
+    
+    init(mile : Double) {
+        self.mile = mile
+    }
+}
+
+class Adapter: MeasureOfLength {
+    private let target: AmericanMeasureOfLength
+    
+    var kilometer: Double {
+        return target.mile * 1.60934
+    }
+    
+    init(target: AmericanMeasureOfLength) {
+        self.target = target
+    }
+}
+
+let km = EuropeanMeasureOfLength(kilometer: 5)
+
+let mi = AmericanMeasureOfLength(mile: 2)
+let adaptedMi = EuropeanMeasureOfLength(kilometer: Adapter(target: mi).kilometer)
